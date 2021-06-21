e25889037ba8c377dc64924ea0111cc749c50fd1
hadeoh <usthmandanfodio@gmail.com>
2021-06-21 22:23:16 +0100

    Add project dependencies and configurations

---
+++ src/main/java/com/webbfontaine/githubanalysis/GithubAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.githubanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webbfontaine/githubanalysis/GithubAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webbfontaine.githubanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
